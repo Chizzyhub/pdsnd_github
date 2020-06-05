189332228a88bbd4a394aa3a4099e914c2f88982
Chidinma Nwajagu <chidimmanwajagu@gmail.com>
2020-06-05 17:17:51 +0100

    Add print statement to main

--- bikeshare.py
+++ bikeshare.py
@@ -235,6 +235,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no: \n')
         if restart.lower() != 'yes':
+            print("Thanks for your time!")
             break
 
 
